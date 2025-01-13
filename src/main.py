d1277422bcfe8a70481de29098725973a5af26e4
d-shames3 <dshames3@gmail.com>
2025-01-13 13:23:12 -0500

    parameterizing path to repo and updating README

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from site_generator import copy_static, generate_pages_recursive
+import os
 
 def main():
-    source = "/Users/david/projects/boot/github.com/d-shames3/static-sites/static"
-    destination = "/Users/david/projects/boot/github.com/d-shames3/static-sites/public"
+    path_to_repo = os.getcwd() 
+    source = os.path.join(path_to_repo, "static")
+    destination = os.path.join(path_to_repo, "public")
+    print(source, destination)
     copy_static(source, destination)
 
-    from_directory = "/Users/david/projects/boot/github.com/d-shames3/static-sites/content"
-    template_path = "/Users/david/projects/boot/github.com/d-shames3/static-sites/template.html"
-    dest_directory = "/Users/david/projects/boot/github.com/d-shames3/static-sites/public"
+    from_directory = os.path.join(path_to_repo, "content")
+    template_path = os.path.join(path_to_repo, "template.html")
+    dest_directory = os.path.join(path_to_repo, "public")
     generate_pages_recursive(from_directory, template_path, dest_directory)
 
 if __name__ == "__main__":
