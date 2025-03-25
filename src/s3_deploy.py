dbd8887cef4670bd65b9540d73662e82cfb1a739
d-shames3 <dshames3@gmail.com>
2025-03-25 19:09:50 -0400

    fixing env vars and deploy script

--- src/s3_deploy.py
+++ src/s3_deploy.py
@@ -1,6 +1,5 @@
 from s3_push import empty_s3_bucket, upload_directory_to_s3_recursive
 import os
-from dotenv import load_dotenv
 import boto3
 
 def main():
@@ -9,16 +8,9 @@ def main():
     destination = os.path.join(path_to_repo, "public")
 
     # load env vars
-    load_dotenv(dotenv_path='.envrc')
-    aws_access_key_id=os.environ.get("aws_access_key_id"),
-    aws_secret_access_key=os.environ.get("aws_secret_access_key")
     bucket=os.environ.get("bucket")
 
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
-    )
+    s3 = boto3.client('s3')
     empty_s3_bucket(s3, bucket)
     upload_directory_to_s3_recursive(s3, bucket, destination)
 
